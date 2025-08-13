0f7a6ed1ca3e8ad1d2b1e60e19b747f855520d91
Hossein Dehghanpoor <hossein@Hosseins-MacBook-Air.local>
2025-08-13 14:33:09 +0330

    first push :)

---
+++ roles/02-terraform/files/terraform_output/terraform.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "vsphere" {
+  user           = "${var.user}"
+  password       = "${var.password}"
+  vsphere_server = "${var.vsphere_server}"
+
+  allow_unverified_ssl = true
+}
+#output "cloud_init_file_content" {
+#  value = file(var.cloud_init_file)
+#}
+data "vsphere_datacenter" "dc" {
+  name = "${var.datacenter}"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "${var.datastore}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "${var.resource_pool}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${var.network}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.template}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "${var.name}"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = "${var.cpus}"
+  memory   = "${var.memory}"
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    #size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    size             = "${var.disk_size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  cdrom {
+    client_device = true
+  }
+
+  vapp {
+    properties ={
+      hostname = var.name
+      user-data = "${base64encode(file(var.cloud_init_file))}"
+    }
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+  }
+  extra_config = {
+    "tools.setPowerOn" = "TRUE"
+  }
+}
+output "vm_ip" {
+  value = vsphere_virtual_machine.vm.default_ip_address
+  description = "IP address of the created VM"
+}
